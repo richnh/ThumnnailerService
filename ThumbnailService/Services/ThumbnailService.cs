8515cf7f2165b0ddc948aad9606cc7239da23f7a
Richard Hale <richard.hale@adgistics.com>
2019-10-07 13:21:04 +0100

    Get ratio working correctly.

--- ThumbnailService/Services/ThumbnailService.cs
+++ ThumbnailService/Services/ThumbnailService.cs
@@ -24,34 +24,26 @@ namespace ThumbnailService.Services
             var masterFilePath    = request.MasterFilePath;
             var thumbNailFilePath = request.ThumbnailFilePath;
 
-            // Figure out the ratio
             double ratioX = (double)thumbnailWidth / (double)masterFileWidth;
             double ratioY = (double)thumbnailHeight / (double)masterFileHeight;
 
-            // use whichever multiplier is smaller
             double ratio = ImageHelpers.CalculateRatioResizingOfImage(request);
           
-            // now we can get the new height and width
-            int newHeight = Convert.ToInt32(masterFileHeight * ratio);
-            int newWidth = Convert.ToInt32(masterFileWidth * ratio);
-
-            // Now calculate the X,Y position of the upper-left corner 
-            // (one of these will always be zero)
-            int posX = Convert.ToInt32((thumbnailWidth - (masterFileWidth * ratio)) / 2);
-            int posY = Convert.ToInt32((thumbnailHeight - (masterFileHeight * ratio)) / 2);
+            int thumbnailCalculatedHeight = Convert.ToInt32(masterFileHeight * ratio);
+            int thumbnailCalculatedWidth = Convert.ToInt32(masterFileWidth * ratio);
 
             using (FileStream pngStream = new FileStream(masterFilePath, FileMode.Open, FileAccess.Read))
 
             using (var image = new Bitmap(pngStream))
             {
-                var thumbNailImage = new Bitmap(thumbnailWidth, thumbnailHeight);
+                var thumbNailImage = new Bitmap(thumbnailCalculatedWidth, thumbnailCalculatedHeight);
 
                 using (var graphics = Graphics.FromImage(thumbNailImage))
                 {
                     graphics.CompositingQuality = CompositingQuality.HighSpeed;
-                    graphics.InterpolationMode  = InterpolationMode.HighQualityBicubic;
-                    graphics.CompositingMode    = CompositingMode.SourceCopy;
-                    graphics.DrawImage(image, posX, posY, newWidth, newHeight);
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.CompositingMode = CompositingMode.SourceCopy;
+                    graphics.DrawImage(image, 0, 0, thumbnailCalculatedWidth, thumbnailCalculatedHeight);
                     thumbNailImage.Save(thumbNailFilePath, ImageFormat.Png);
                 }
             }
